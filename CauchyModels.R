0294ebce6ece82ce63a859c8da39dc2699ea736b
PKraska02 <pkraska02@gmail.com>
2025-06-05 18:49:23 +0200

    R models push

---
+++ CauchyModels.R
@@ -0,0 +1,48 @@
+# Wymagane biblioteki
+library(e1071)    
+library(caret)     
+
+source("generate_couchy.R")
+data <- generate_cauchy_data(n = 100, center1 = c(-5, -3), center2 = c(3, 5))
+
+# 2. Grou jako kategorie
+data$group <- factor(data$group)
+
+set.seed(13)
+train_idx <- createDataPartition(data$group, p = 0.7, list = FALSE)
+train_data <- data[train_idx, ]
+test_data  <- data[-train_idx, ]
+
+
+## SVM
+svm_model <- svm(group ~ ., data = train_data, kernel = "radial")
+svm_pred <- predict(svm_model, test_data)
+
+## Naive Bayes
+nb_model <- naiveBayes(group ~ ., data = train_data)
+nb_pred <- predict(nb_model, test_data)
+
+# SVM metryki
+cat("\n=== SVM ===\n")
+cm_svm <- confusionMatrix(svm_pred, test_data$group)
+print(cm_svm)
+
+precision_svm <- cm_svm$byClass["Precision"]
+recall_svm    <- cm_svm$byClass["Recall"]
+f1_svm <- 2 * (precision_svm * recall_svm) / (precision_svm + recall_svm)
+cat(sprintf("F1-score (SVM): %.3f\n", f1_svm))
+
+#Bayes metryki
+cat("\n=== Naive Bayes ===\n")
+cm_nb <- confusionMatrix(nb_pred, test_data$group)
+print(cm_nb)
+
+precision_nb <- cm_nb$byClass["Precision"]
+recall_nb    <- cm_nb$byClass["Recall"]
+f1_nb <- 2 * (precision_nb * recall_nb) / (precision_nb + recall_nb)
+cat(sprintf("F1-score (Naive Bayes): %.3f\n", f1_nb))
+
+#saveRDS(svm_model, "svm_model_Cauchy.rds")
+#saveRDS(nb_model, "nb_model_Cauchy.rds")
+
+
